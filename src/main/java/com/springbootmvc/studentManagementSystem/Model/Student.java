a7171c6850df340a8a27a6a1cc9d61d078a2cfb2
Ganesh Parmar <ganeshparmar221997@gmail.com>
2022-04-28 19:56:16 +0530

    [SpringBoot MVC App] Added Student Entity and pom pump up

---
+++ src/main/java/com/springbootmvc/studentManagementSystem/Model/Student.java
@@ -0,0 +1,78 @@
+package com.springbootmvc.studentManagementSystem.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name",nullable=false)
+	private String firstName;
+	
+	@Column(name="last_name",nullable=false)
+	private String lastName;
+	
+	@Column(name="email",nullable=false)
+	private String email;
+	
+	@Column(name="address",nullable=false)
+	private String address;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public Student(int id, String firstName, String lastName, String email, String address) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+	}
+	
+	public Student() {
+		super();
+		
+	}
+	
+	
+	
+}
